efb922d8473c4463e9627fb16d4df61fd9a92e3e
Greg Gyetko <ggyetko@gmail.com>
2022-12-02 14:28:16 +0000

    AoC2022 day2 part2

---
+++ advent2022_d2_p2.py
@@ -0,0 +1,17 @@
+import re
+
+resultDict = {"X":0, "Y":1, "Z":2}  # array position for lose/draw/win (also, points for same if you x3)
+calcDict = {"A":["C","A","B"], "B":["A","B","C"], "C":["B","C","A"]} # map lose/draw/win into this array
+pointsDict = {"A":1,"B":2,"C":3}
+
+file = open("advent2022_data02.txt","r")
+
+pts = 0
+for line in file:
+    them = line[0]
+    me = calcDict[them][resultDict[line[2]]]
+    pts += pointsDict[me] + resultDict[line[2]] * 3
+
+print ("Total points:",pts)
+
+    
